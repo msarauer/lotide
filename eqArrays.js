9e21229af30c09f3402c5023a02141817ecae69d
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-04-27 16:05:04 -0600

    ran eslint on eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length){
+  if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
@@ -16,6 +16,6 @@ const eqArrays = function(arr1, arr2) {
     }
   }
   return true;
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 1]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 1]), true);
