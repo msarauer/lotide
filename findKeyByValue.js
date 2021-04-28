100f88e8c6b7afd9d59d7d6a5c81ea5b338c0e7a
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-04-28 14:44:41 -0600

    Added findKeyByValue.js which finds the key that an inputted value is first seen at

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  for (const item in obj) {
+    if (obj[item] === val) {
+      return item;
+    }
+  }
+  return;
+};
+
+//TEST SCENARIOS
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
