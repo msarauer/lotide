ad1fa77986edece4df1b20b70575501326b78290
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-05-03 13:05:15 -0600

    Created a test folder with the assertEqual and head tests and removed the test codes from the original files.

--- head.js
+++ head.js
@@ -1,14 +1,8 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
