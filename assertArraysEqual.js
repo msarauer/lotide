0c0433cabd6f14a862d9088e69d3d0cbd478a314
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-05-03 13:26:24 -0600

    Moved the test functionality from the assertArraysEqual file to the test directory

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`👌👌👌 Assertion Passed: ${arr1} === ${arr2}`);
@@ -6,16 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+module.exports = assertArraysEqual;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
